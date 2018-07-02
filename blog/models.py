f76e230dd3513b34be107614484e53b094a153fa
Reza <rezahnamavar@gmail.com>
2018-07-02 22:44:15 +0200

    Full first

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
+
+# Create a Blog models
+# title
+# pub_date
+# body
+# image
+
+
+# Add the Blog app to the settings
+#create a migration
+#Migrate
+#Add to the admin
